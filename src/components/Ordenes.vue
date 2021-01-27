0758bbefffcb45f7db08849065d8b8fd4c51dcbc
Christian <cjvaldivieso@hotmail.com>
2021-01-27 18:41:08 -0500

    Con vuetify y pestañas

---
+++ src/components/Ordenes.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+  <v-tabs
+    color="cyan"
+    dark
+    slider-color="yellow"
+    >
+    <v-tab ripple>
+      Ordenes
+    </v-tab>
+    <v-tab ripple>
+      Otras Ordenes
+    </v-tab>
+    <v-tab ripple>
+      Personas
+    </v-tab>
+    <v-tab ripple>
+      Cuentas
+    </v-tab>
+    <v-tab-item>
+      <v-card flat>
+        <v-card-text>Mostrando Ordenes</v-card-text>
+      </v-card>
+    </v-tab-item>
+    <v-tab-item>
+      <v-card flat>
+        <v-card-text>Mostrando Otras Ordenes</v-card-text>
+      </v-card>
+    </v-tab-item>
+    <v-tab-item>
+      <v-card flat>
+        <v-card-text>Mostrando Personas</v-card-text>
+      </v-card>
+    </v-tab-item>
+    <v-tab-item>
+      <v-card flat>
+        <v-card-text>Mostrando Cuentas</v-card-text>
+      </v-card>
+    </v-tab-item>
+    </v-tabs>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Ordenes',
+
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+  }
+</script>
